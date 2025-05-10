66533636fdb98982542ee07881db2e4991c13edd
hiroto <tohhiro@gmail.com>
2025-05-10 12:40:38 +0900

    zodSchemaを追加しフォームへ実装

---
+++ app/components/Form/helpers/validationSchema/index.test.ts
@@ -0,0 +1,45 @@
+import { describe, expect, it } from "vitest";
+import { validationSchema } from ".";
+
+const createFileListMock = (files: File[]): FileList => {
+  return {
+    0: files[0],
+    length: files.length,
+    item(index: number) {
+      return (this as unknown as FileList)[index] ?? null;
+    },
+    [Symbol.iterator]: function* (this: FileList) {
+      for (let i = 0; i < this.length; i++) {
+        yield this[i];
+      }
+    },
+  } as unknown as FileList;
+};
+
+describe("validationSchema", () => {
+  test("CSVであればtrue", () => {
+    const csvFile = new File(["id,name\n1,Alice"], "test.csv", {
+      type: "text/csv",
+    });
+    const fileList = createFileListMock([csvFile]);
+
+    const result = validationSchema.safeParse({ file: fileList });
+    expect(result.success).toBe(true);
+  });
+
+  test("ファイルが存在しない場合はfalse", () => {
+    const fileList = createFileListMock([]);
+    const result = validationSchema.safeParse({ file: fileList });
+    expect(result.success).toBe(false);
+  });
+
+  test("CSVでない場合はfalse", () => {
+    const txtFile = new File(["Hello"], "test.txt", {
+      type: "text/plain",
+    });
+    const fileList = createFileListMock([txtFile]);
+
+    const result = validationSchema.safeParse({ file: fileList });
+    expect(result.success).toBe(false);
+  });
+});
