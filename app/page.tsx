1b4a79e6cbe1858ad37d6c566be5b8a42cf45b6d
hiroto <tohhiro@gmail.com>
2025-06-01 11:37:34 +0900

    ページタイトルとヘッダーを修正

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const Home: FC = () => {
     <main>
       <div className="flex justify-center items-center h-screen">
         <div className="flex flex-col items-center">
-          <h1 className="text-2xl font-bold">Hello World</h1>
+          <h1 className="text-2xl font-bold">CSV encrypt</h1>
           <Form />
         </div>
       </div>
